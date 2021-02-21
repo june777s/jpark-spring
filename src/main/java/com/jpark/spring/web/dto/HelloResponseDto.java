4fd5d09fd2eca9e4e840b0c4878c9953e0455809
jpark <june777s@naver.com>
2021-02-22 01:41:04 +0900

    롬복까지 완료

--- src/main/java/com/jpark/spring/web/dto/HelloResponseDto.java
+++ src/main/java/com/jpark/spring/web/dto/HelloResponseDto.java
@@ -3,6 +3,7 @@ package com.jpark.spring.web.dto;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+// 롬복은 자바개발시 자주 사용하는 코드 Getter, Setter , 기본생성자, toString등을 어노테이션으로 자동 생성하게 해줌
 @Getter //1.선언된 모든 필드의 get 메소드를 생성
 @RequiredArgsConstructor //2. 선언된 모든 final  필드가 포함된 생성자를 생성해 줌
                          //   final이 없는 필드는생성자에 포함되지 않음
--- src/test/java/com/jpark/spring/web/HelloControllerTest.java
+++ src/test/java/com/jpark/spring/web/HelloControllerTest.java
@@ -6,26 +6,50 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+//얘는 왜 자동 임포트도 안됐을까...?
 import org.springframework.test.web.servlet.ResultActions;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-@RunWith(SpringRunner.class)//
-@WebMvcTest(controllers = HelloController.class)//
+@RunWith(SpringRunner.class)//테스트를 진행할때 Junit에 내장된 실행자 외에 다른 실행자를 실행시킴
+                            //여기서는 SpringRunner라는 스프링 실행자를 사용
+                            //스프링부트 테스트와 JUnit 사이에 연결자 역할을 함
+@WebMvcTest(controllers = HelloController.class)
+//여러 스프링 테스트 어노테이션 중, web(Spring MVC)에 집중할 수 있는 어노테이션
+//선언할 경우 Controller,ControllerAdvice등 어노테이션 사용 가능
+// service, Component, Repository 어노테이션은 사용 불가
 public class
 HelloControllerTest {
 
-    @Autowired
-    private MockMvc mvc;
+    @Autowired //스프링이 관리하는 빈을 주입
+    private MockMvc mvc;// 웹 api를 테스트 할떄 사용 , 스프링 mvc테스트의 시작점
+                        // 이클래스를 통해 HTTP GET, POST 등에 대한 API 테스트를 할 수 있
 
     @Test
     public void hello가_리턴된다() throws Exception{
         String hello = "hello";
 
-        mvc.perform(get("/hello"))
-                .andExpect(status().isOk())//
+        mvc.perform(get("/hello")) //MockMvc를 통하여 /hello 주소로 HTTP GET 요청을 함
+                .andExpect(status().isOk())// 체이닝 지원이 되어 여러 검증 기능이 이렇게 넣을수 있음 ex> .andExpect() <--이렇게
+                                           // mvc.perform의 결과를 검증 ,HTTP Head의 Status를 검증
+                                           // 흔히 알고 있는 200, 404, 500 등상태를 검증 여기서는 ok(200) 인지에 대한 검증
                 .andExpect(content().string(hello));
+                                            // mvc.perform의 결과 검증
+                                            // 응답 본문의 내용 검증 값 비교 ( string 타입의 값)
+    }
+    @Test
+    public void helloDto가_리턴된다() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto").param("name",name)//
+                                                .param("amount",String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name",is(name))) //
+                .andExpect(jsonPath("$.amount",is(amount)));
     }
 }
 
